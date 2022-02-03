f99708e4718924a839cc126cec0d91c40bec5ffc
Hassan Askar <hassanemad15697@gmail.com>
2022-02-03 08:36:12 +0100

    get spesific data by URL with @PathVariable

--- src/main/java/com/spring/SpringRESTful/Controller/MainController.java
+++ src/main/java/com/spring/SpringRESTful/Controller/MainController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +32,19 @@ public class MainController {
 		students.add(new Student(3, "Esraa", "Italy/Roma", 21));
 		students.add(new Student(4, "Hager", "Italy/Milano",19));
 		return students;
+	}
+	// http://localhost:8080/SpringRESTful/get-student/{studentId}
+	@GetMapping("/get-student/{studentId}")
+	public Student fwrStudentById( @PathVariable("studentId") int id)
+	{
+		
+		List<Student> students = new ArrayList<Student>() ; 
+		students.add(new Student(1, "Hassan", "Egypt/Cairo", 24));
+		students.add(new Student(2, "Amro", "Egypt/Sharkia", 22));
+		students.add(new Student(3, "Esraa", "Italy/Roma", 21));
+		students.add(new Student(4, "Hager", "Italy/Milano",19));
+		
+		
+		return students.get(id-1);
 	}
 }
